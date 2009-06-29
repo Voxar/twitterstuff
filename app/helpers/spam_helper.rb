348139b8f0ccf33199c92fb27eb99a249b653c2d
Patrik Sjöberg <voxxar@vMac.local>
2009-06-29 19:26:27 +0200

    block or follow mediums

--- app/helpers/spam_helper.rb
+++ app/helpers/spam_helper.rb
@@ -5,16 +5,24 @@ module SpamHelper
     when 0.0..0.1: "<span class=\"bad\">BAD</span> <span class=\"rating\">(#{follower.rating.round(2)})</span>"
     when 0.1..0.3: "<span class=\"medium\">Medium</span> <span class=\"rating\">(#{follower.rating.round(2)})</span>"
     when 0.3..1.0: "<span class=\"good\">Good</span> <span class=\"rating\">(#{follower.rating.round(2)})</span>"
-    when 1.0..2.0: "<span class=\"good\">Great</span> <span class=\"rating\">(#{follower.rating.round(2)})</span>"
-    else "<span class=\"whale\">Whale!</span> <span class=\"rating\">(#{follower.rating.round(2)})</span>"
+    else "<span class=\"good\">Great</span> <span class=\"rating\">(#{follower.rating.round(2)})</span>"
+#    else "<span class=\"whale\">Whale!</span> <span class=\"rating\">(#{follower.rating.round(2)})</span>"
     end
+  end
+  
+  def link_to_block follower, text='Block this user'
+    "#{link_to_remote text, :url=>{:action=>'block', :id=>follower.id}, :update=>"suggestion_#{follower.id}"}"
+  end
+  
+  def link_to_follow follower, text='follow this user'
+    "#{link_to_remote text, :url=>{:action=>'follow', :id=>follower.id}, :update=>"suggestion_#{follower.id}"}"
   end
   
   def rating_suggestion follower
     case follower.rating
-    when 0.0..0.1: "This is probably a spammer. #{link_to_remote 'Block this user', :url=>{:action=>'block', :id=>follower.id}, :update=>"suggestion_#{follower.id}"}"
-    when 0.1..0.3: "This might be a spammer. <a href=\"http://www.twitter.com/#{follower.screen_name}\" target=\"_blank\">Check out users profile</a> and make a decision"
-    else "This is probably a regular twitterer. You might want to #{link_to_remote 'follow this user', :url=>{:action=>'follow', :id=>follower.id}, :update=>"suggestion_#{follower.id}"}"
+    when 0.0..0.1: "This is probably a spammer. #{link_to_block(follower)}"
+    when 0.1..0.3: "This might be a spammer. <a href=\"http://www.twitter.com/#{follower.screen_name}\" target=\"_blank\">Check users profile</a> and then maybe #{link_to_block(follower, 'block')} or #{link_to_follow(follower, 'follow')}"
+    else "This is probably a regular twitterer. You might want to #{link_to_follow(follower)}"
     end
     
   end
